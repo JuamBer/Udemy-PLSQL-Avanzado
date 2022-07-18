b3a7ef047bd759a610f3d7e68f91de0d124f06f0
Juan Sáez García <jsaez@odec.es>
2022-07-18 15:03:19 +0200

    Como compilar para PL/SQL Nativo

---
+++ clases/05.plsql-nativo/02.sql
@@ -0,0 +1,27 @@
+  ALTER SESSION SET PLSQL_CODE_TYPE='INTERPRETED';
+
+create or replace PROCEDURE N1  AS
+V VARCHAR2(1000):='A';    
+X DATE;
+Z varchar2(1000);
+BEGIN
+     DBMS_OUTPUT.PUT_LINE(to_char(sysdate,'mi:ss'));
+    FOR I IN 1..100000000 LOOP
+        FOR X IN 1..15 LOOP
+            V:='A'||SUBSTR('AAAAAA',1,5);
+        end loop;
+    END LOOP;  
+
+         DBMS_OUTPUT.PUT_LINE(to_char(sysdate,'mi:ss'));
+
+END;
+/
+
+set serveroutput on
+execute n1;
+
+ALTER SESSION SET PLSQL_CODE_TYPE='NATIVE';
+ALTER SESSION SET PLSQL_CODE_TYPE='INTERPRETED';
+/
+
+SELECT * FROM USER_PLSQL_OBJECT_SETTINGS WHERE NAME LIKE 'N1';
